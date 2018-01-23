9dc75947db53d5ffb32d195edd94eea87cd87797
jairoarivas <jairoalbertorivas@gmail.com>
2018-01-23 10:53:49 -0500

    er

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ const app = configureExpress();
 const passport = configurePassport();
 
 // Use the Express application instance to listen to the '3000' port
-app.listen(process.env.PORT || 3000);
+var port = process.env.PORT || 3000;
+app.listen(port);
 
 // Log the server status to the console
 console.log('Server running at http://localhost:3000/');
